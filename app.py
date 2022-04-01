3949a9525c12172cf071c1c6a3b012200d345e79
Elizaveta <falloutgirl7@mail.ru>
2022-04-01 14:25:33 +0300

    Restructure app.py, nutrition.py, results.html

--- app.py
+++ app.py
@@ -1,37 +1,47 @@
 from flask import Flask, render_template, request
 from werkzeug.exceptions import HTTPException
 
-from nutrition import nutrition
+from nutrition import nutrition, resource_of_nutrients
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def entry_page() -> 'html':
-    """Выводит HTML-форму"""
+
     return render_template('entry.html.j2', the_title='Health App')
 
 @app.route('/result', methods=['POST'])
 def data() -> 'html':
-    """Извлекает данные из запроса, записывает в эксель, возвращает результаты""" 
 
     food = request.form['food']
     suppl = request.form.getlist('suppl')
 
-    not_found, lack, resource_of_lack, protein, fat, carbs, calorii = nutrition(food, suppl)
+    not_found, nutrient_dict, calorii = nutrition(food, suppl)
 
-    resource = []
-    for k, v in resource_of_lack.items():
-        v = ', '.join(v)
-        resource.append(f'{k}: {v}.')
+    recommend_amount = {'Магний': 300, 'Цинк': 12, 'B6': 2, 'C': 120, 'P': 30, 
+                        'Селен': 55, 'Йод': 150, 'B1': 1.5, 'B2': 1.8, 'B5': 5, 'B9': 200, 'B12': 2,
+                        'N': 30, 'PP': 10, 'Марганец': 3.8, 'Медь': 2, 'Омега3': 1100,
+                        'Железо': 18, 'Калий': 2500, 'Фосфор': 1000,
+                        'Кальций': 1000, 'E': 15, 'D': 5, 'A': 750, 'K': 120, 'Лютеин': 5000}
+
+    nutrient_lack_list = []
+    for k, v in recommend_amount.items():
+        if nutrient_dict[k] < v * 0.9:
+            nutrient_lack_list.append(k)
+    
+    nutrient_resource = dict(resource_of_nutrients(nutrient_lack_list))
+
 
     return render_template('results.html.j2', 
                             not_found=not_found,
-                            lack=lack,
-                            resource=resource,
-                            protein=protein,
-                            fat=fat, 
-                            carbs=carbs,
+                            nutrient_dict=nutrient_dict,
+                            recommend_amount=recommend_amount,
+                            nutrient_resource=nutrient_resource,
+                            protein=nutrient_dict['Белки'],
+                            fat=nutrient_dict['Жиры'], 
+                            carbs=nutrient_dict['Углеводы'],
                             kkal=calorii)
 
 @app.errorhandler(Exception) 
@@ -41,4 +51,4 @@ def handle_exception(error) -> 'html':
         return entry_page()
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1')
+    app.run()
